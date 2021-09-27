558738ecd80b3ff48ab5dae2cce28ac70bd6226e
SalilThreeplus <salil.luley@threeplus.in>
2021-09-27 13:22:22 -0700

    Added stacked services

---
+++ chaalak/lib/ui/views/home/home_viewmodel.dart
@@ -0,0 +1,7 @@
+import 'package:stacked/stacked.dart';
+
+class HomeViewModel extends BaseViewModel {
+  String _title = "Model";
+
+  String get title => _title;
+}
---
+++ chaalak/lib/ui/views/splash/splash_viewmodel.dart
@@ -0,0 +1,7 @@
+import 'package:stacked/stacked.dart';
+
+class SplashViewModel extends BaseViewModel {
+  String _title = "Model";
+
+  String get title => _title;
+}
