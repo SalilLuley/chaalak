c4e1b6008692763e701105e73771c2fab6210e0f
SalilThreeplus <salil.luley@threeplus.in>
2021-09-28 04:36:29 -0700

    Added animation controller hook

--- chaalak/lib/ui/views/splash/splash_view.dart
+++ chaalak/lib/ui/views/splash/splash_view.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:lottie/lottie.dart';
 import 'package:stacked/stacked.dart';
 import 'splash_viewmodel.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends HookWidget {
   const SplashView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final animationController = useAnimationController();
+
     return ViewModelBuilder<SplashViewModel>.reactive(
       builder: (context, model, child) => Scaffold(
         body: Center(
-          child: Lottie.asset("assets/lottie/delivery.json"),
+          child: Lottie.asset(
+            "assets/lottie/map.json",
+            onLoaded: (composition) {
+              animationController.addStatusListener((status) {
+                if (status == AnimationStatus.completed) {
+                  model.indicateAnimationComplete();
+                }
+              });
+
+              // Configure the AnimationController with the duration of the
+              // Lottie file and start the animation.
+              animationController
+                ..duration = composition.duration
+                ..forward();
+            },
+          ),
         ),
         floatingActionButton:
             FloatingActionButton(onPressed: () => model.navigateToDetail()),
--- chaalak/lib/ui/views/splash/splash_viewmodel.dart
+++ chaalak/lib/ui/views/splash/splash_viewmodel.dart
@@ -5,11 +5,23 @@ import 'package:stacked_services/stacked_services.dart';
 
 class SplashViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
+  bool _animationComplete = false;
+
   String _title = "Splash";
 
   String get title => _title;
 
   navigateToDetail() {
     _navigationService.navigateTo(Routes.homeView);
+  }
+
+  Future _replaceWith() async {
+    Future.delayed(Duration(seconds: 0)).then((value) async =>
+        {await _navigationService.replaceWith(Routes.homeView)});
+  }
+
+  Future indicateAnimationComplete() async {
+    _animationComplete = true;
+    await _replaceWith();
   }
 }
