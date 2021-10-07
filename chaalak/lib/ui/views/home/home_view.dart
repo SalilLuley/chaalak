2bf7313b2391b8dbf1d5ba2c26aeeaf491016ccd
SalilThreeplus <salil.luley@threeplus.in>
2021-10-07 12:47:30 -0700

    Added image

--- chaalak/lib/ui/views/home/home_view.dart
+++ chaalak/lib/ui/views/home/home_view.dart
@@ -10,63 +10,77 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ViewModelBuilder<HomeViewModel>.reactive(
       builder: (context, model, child) => Scaffold(
-        body: Center(
-          child: ElevatedButton(
-            child: const Text('showModalBottomSheet'),
-            onPressed: () {
-              showModalBottomSheet<void>(
-                context: context,
-                builder: (BuildContext context) {
-                  return Container(
-                    color: Colors.amberAccent,
-                    height: MediaQuery.of(context).size.height / 2,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Container(
-                          margin: const EdgeInsets.only(top: 10),
-                          height: MediaQuery.of(context).size.height / 3.5,
-                          child: ListView(
-                            scrollDirection: Axis.horizontal,
-                            children: <Widget>[
-                              Container(
-                                width: 160.0,
-                                color: Colors.red,
-                              ),
-                              Container(
-                                width: 160.0,
-                                color: Colors.blue,
-                              ),
-                              Container(
-                                width: 160.0,
-                                color: Colors.green,
-                              ),
-                              Container(
-                                width: 160.0,
-                                color: Colors.yellow,
-                              ),
-                              Container(
-                                width: 160.0,
-                                color: Colors.orange,
-                              ),
-                            ],
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          centerTitle: true,
+          title: Text("Chaalak"),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+                child: Image(
+              image: AssetImage("assets/images/google-map.png"),
+              fit: BoxFit.fill,
+            )),
+            ElevatedButton(
+              child: const Text('showModalBottomSheet'),
+              onPressed: () {
+                showModalBottomSheet<void>(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Container(
+                      color: Colors.amberAccent,
+                      height: MediaQuery.of(context).size.height / 2,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Container(
+                            margin: const EdgeInsets.only(top: 10),
+                            height: MediaQuery.of(context).size.height / 3.5,
+                            child: ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: <Widget>[
+                                Container(
+                                  width: 160.0,
+                                  color: Colors.red,
+                                ),
+                                Container(
+                                  width: 160.0,
+                                  color: Colors.blue,
+                                ),
+                                Container(
+                                  width: 160.0,
+                                  color: Colors.green,
+                                ),
+                                Container(
+                                  width: 160.0,
+                                  color: Colors.yellow,
+                                ),
+                                Container(
+                                  width: 160.0,
+                                  color: Colors.orange,
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(top: 30),
-                          child: ElevatedButton(
-                            child: const Text('Close BottomSheet'),
-                            onPressed: () => Navigator.pop(context),
-                          ),
-                        )
-                      ],
-                    ),
-                  );
-                },
-              );
-            },
-          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 30),
+                            child: ElevatedButton(
+                              child: const Text('Close BottomSheet'),
+                              onPressed: () => Navigator.pop(context),
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ],
         ),
       ),
       viewModelBuilder: () => HomeViewModel(),
